69cadb5e4b8c924261e3f8d93de22a1b9613ba8d
bouka <bouka@LAPTOP-KBP8PI8H>
2021-01-28 14:11:56 +0100

    Primera prueba git

---
+++ estructuras_dinamicas/src/pilas_enteros/hola.java
@@ -0,0 +1,11 @@
+package pilas_enteros;
+
+public class hola {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("prueba de git");
+
+	}
+
+}
